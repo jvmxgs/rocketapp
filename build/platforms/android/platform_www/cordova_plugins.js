74766b2dc50f1739b0fe1f68192a696dd82636f9
Juan Victor <jvmxgs@gmail.com>
2019-06-11 10:00:32 -0500

    quitando archivos

--- build/platforms/android/platform_www/cordova_plugins.js
+++
@@ -1,6 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
-  module.exports.metadata = {
-    "cordova-plugin-browsersync": "0.1.7"
-  };
-});
